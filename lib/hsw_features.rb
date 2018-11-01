c6e8f762447d816298871cc86c4facfcc44b24e5
Mohammed Noori <kmanoorib@gmail.com>
2018-11-01 00:24:11 +0000

    Add environment file: hsw_features.rb.

---
+++ lib/hsw_features.rb
@@ -0,0 +1,6 @@
+require_relative "./hsw_features/articles.rb"
+require_relative "./hsw_features/cli.rb"
+require_relative "./hsw_featuresscraper.rb"
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
